3782387a1aec7d9872cdf950643bd6ce53516968
CodeCraftsfolk <bytebard8@gmail.com>
2023-09-26 17:47:50 +0800

    feat: un comment status page redirect logic

--- web/src/pages/sharedStatus/SharedStatus.tsx
+++ web/src/pages/sharedStatus/SharedStatus.tsx
@@ -78,8 +78,7 @@ const SharedStatus = () => {
 
         const hostSharedLink = fileInfo.host_shared_link;
         if (state === "OK" && hostSharedLink) {
-          // TODO:
-          // location.href = hostSharedLink;
+          location.href = hostSharedLink;
         }
       }
 
